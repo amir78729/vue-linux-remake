a275013ed0a5fc81cd510f4a03479de896e5f0cd
amir78729 <amir78729@gmail.com>
2020-08-06 00:42:00 +0430

    workshop item created (hardcoded)

--- src/components/WorkshopGrid.vue
+++ src/components/WorkshopGrid.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h2>کارگاه‌ها و سخنرانی‌ها</h2>
-    <div class="row container bg-danger">
-
+    <div class="row align-content-center" style="padding: 0 20px 0 20px">
+      <div class="col-md-6 col-sm-12">
+        <grid-workshop-item></grid-workshop-item>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <grid-workshop-item ></grid-workshop-item>
+      </div>
     </div>
   </div>
 </template>
+<script>
+  import WorkshopItem from "./WorkshopItem.vue";
 
+  export default {
+    components: {
+      gridWorkshopItem: WorkshopItem,
+    }
+  }
+</script>
 <style scoped>
   h2 {
     margin: 50px;
---
+++ src/components/WorkshopItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="item">
+    <h4 style="font-weight: bolder;">{{ title }}</h4>
+    <h5 v-for="name in names">{{ name }}</h5>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'عنوان',
+        names: ['name1', 'name2', 'name3']
+      }
+    },
+  }
+</script>
+
+<style>
+  .item {
+    background-color: #292929;
+    color: white;
+    transition: all 0.3s;
+    border-radius: 10px;
+    padding: 30px;
+  }
+  .item:hover {
+    background-color: #521C39;
+    color: #e4b22b;
+    box-shadow: 0 0 5px 0 black;
+    border-radius: 15px;
+  }
+</style>
