2d6ce0ce32b6d4e61794a2c90890ca020f893751
amir78729 <amir78729@gmail.com>
2020-08-05 21:05:54 +0430

    convert header to a component

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,18 @@
 <template>
-
-  <div class="">
-    <nav class="navbar header">
-      <div class="collapse navbar-collapse d-flex header container-fluid" id="navbarSupportedContent">
-        <div class="row col-md-8">
-          <button class="btn header-register-btn">ثبت نام کارگاه‌ها</button>
-          <div class="header-divider"></div>
-          <button class="btn header-regular-btn">ورود به حساب کاربری</button>
-        </div>
-        <div class="col-md-4 row">
-          <button class="btn header-regular-btn">کارگاه‌ها و سخنرانی‌ها</button>
-          <div class="header-divider"></div>
-          <button class="btn header-regular-btn">درباره جشنواره</button>
-          <div class="header-divider"></div>
-          <button class="btn header-regular-btn">خانه</button>
-        </div>
-
-      </div>
-    </nav>
+  <div>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
+  import Header from "./components/Header.vue";
+  export default {
+    components: {
+      appHeader: Header
+    }
 }
 </script>
 
 <style>
-  .header {
-    background-color: #521C39;
-  }
-  .header-divider {
-    border-left: 2px solid #e4b22b;
-    width: 1px;
-    margin: 0 3px 0 3px;
-  }
-  .header-register-btn {
-    margin-right: 15px;
-    background-color: #e4b22b;
-    font-size: 14px;
-    height: 40px;
-    transition: all 0.3s;
 
-  }
-  .header-register-btn:hover {
-    background-color: white;
-    color: #521C39;
-    border-radius: 20px;
-    transition: all 0.3s;
-  }
-  .header-regular-btn {
-    color: white;
-    font-size: 14px;
-  }
-  .header-regular-btn:hover{
-    color: #e4b22b;
-    font-size: 14px;
-  }
 </style>
